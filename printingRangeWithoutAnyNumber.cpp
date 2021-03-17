d2c24a3cacf7a3c0e763425b0d7f6ff5db36ce6b
Krishna Singh <69842389+Krishna-Singh99@users.noreply.github.com>
2021-03-17 14:34:08 +0530

    Update printingRangeWithoutAnyNumber.cpp

--- printingRangeWithoutAnyNumber.cpp
+++ printingRangeWithoutAnyNumber.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    char ch='d';
-    char c='c';
-    int x=int(ch)-int(c);
-    do{
+    char ch='d'; //ascii value 100
+    char c='c';  //ascii value 99
+    int x=int(ch)-int(c);  //x value will be 1 at initial
+    do{                    // loop until reaches 101
     cout<<x <<" ";
     x++;
     }
-    while(x!=ch+1);
+    while(x!=ch+1);    //end of loop
     return 0;
 }
